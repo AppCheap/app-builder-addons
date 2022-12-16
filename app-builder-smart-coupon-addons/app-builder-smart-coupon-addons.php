9d5320e75ceab371f63144668101890f8b42de09
dangtienngoc <dangtienngoc@hotmail.com>
2022-12-16 09:45:55 +0700

    feat: add is_invalid

--- app-builder-smart-coupon-addons/app-builder-smart-coupon-addons.php
+++ app-builder-smart-coupon-addons/app-builder-smart-coupon-addons.php
@@ -67,7 +67,7 @@ function app_builder_smart_coupon_list( $request ) {
 		$invalid = false;
 
 		// Force show coupon like minimum order total
-		if ( 'woocommerce_before_my_account' !== current_filter() && ! $coupon->is_valid() && false ) {
+		if ( 'woocommerce_before_my_account' !== current_filter() && ! $coupon->is_valid() ) {
 
 			// Filter to allow third party developers to show coupons which are invalid due to cart requirements like minimum order total or products.
 			$wc_sc_force_show_coupon = apply_filters( 'wc_sc_force_show_invalid_coupon', false, array( 'coupon' => $coupon ) );
